0acd6ba99b4cab06d43621c8eb2846f14b8aa65b
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-03-20 20:55:44 +0530

    integrating_CRUD_with_MangoDB

--- models/product.js
+++ models/product.js
@@ -1,24 +1,76 @@
 
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product{
-  constructor(title , price , description , imageUrl){
+  constructor(title , price , description , imageUrl ,id){
   this.title = title ;
   this.price = price;
   this.description = description;
   this.imageUrl = imageUrl;
+  this._id = id ? new mongodb.ObjectId(id) : null ;
   }
 
   save(){
   const db = getDb();
-  return db.collection('product')
-  .insertOne(this)
+  let dbOp ;
+  if(this._id){
+    // update the product
+    dbOp = db
+    .collection('products')
+    .updateOne({_id:this._id},{$set:this});// this represent the entire class constructor params 
+  }else{
+     dbOp = db.collection('products')
+     .insertOne(this)
+  }
+  return dbOp
   .then(result =>{
     console.log(result)
   })
   .catch(err =>{
     console.log(err)
   })
+  }
+  static fetchAll(){
+    let db = getDb();
+    return db.collection('products')
+    .find()// find all the products give the next document after one documents form the mongoDB database
+    .toArray()// turn them in to javascript array with toArray method
+    .then( products =>{
+      console.log(products);
+      return products // return the products 
+    })
+    .catch(err =>{
+      console.log(err);
+      throw new Error(err)
+    })
+  }
+  static findByPk(prodId){
+    let db = getDb();
+    return db.collection('products')
+    .find({_id:new mongodb.ObjectId(prodId)})
+    .next()
+    .then(product=>{
+      console.log(product);
+      return product ;
+    })
+    .catch(err =>{
+      console.log(err)
+      throw new Error(err)
+    })
+  }
+  static deleteByPk(prodId){
+    let db = getDb();
+    db.collection('products')
+    .deleteOne({_id: new mongodb.ObjectId(prodId)})
+    .then(result =>{
+      console.log(result);
+      return result;
+    })
+    .catch(err =>{
+      console.log(err);
+      throw new Error(err)
+    })
   }
 }
 
