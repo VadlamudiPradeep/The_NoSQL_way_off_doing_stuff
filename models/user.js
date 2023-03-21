9aec02ee028e95e68cfd2f213d6c9e99ca9bb3b5
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-03-21 10:37:53 +0530

    Create_user_table

--- models/user.js
+++ models/user.js
@@ -1,16 +1,37 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+class Product{
+  constructor(name, email , phone, password ,id){
+  this.name = name ;
+  this.email = email ;
+  this.phone =phone;
+  this.password = password;
+  this._id = id ? new mongodb.ObjectId(id) : null ;
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save(){
+  const db = getDb();
+  let dbOp ;
+  if(this._id){
+    // update the product
+    dbOp = db
+    .collection('user')
+    .updateOne({_id:this._id},{$set:this});// this represent the entire class constructor params 
+  }else{
+     dbOp = db.collection('user')
+     .insertOne(this)
+  }
+  return dbOp
+  .then(result =>{
+    console.log(result)
+  })
+  .catch(err =>{
+    console.log(err)
+  })
+  }
+
+}
+
 
 module.exports = User;
