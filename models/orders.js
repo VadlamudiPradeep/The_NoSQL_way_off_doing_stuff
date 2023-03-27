79f7bc7c0ab43af4b3e7ce3442a7407d03b2bafc
Vandlamudi Pradeep <vandlamudipradeepchoudhary@gmail.com>
2023-03-27 12:25:13 +0530

    No_SQL_complete_showdown

--- models/orders.js
+++ models/orders.js
@@ -1,29 +1,25 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema ; /* to construct the schema
-it allows to create new schemas .
-*/
+
+const Schema = mongoose.Schema;
+
 const orderSchema = new Schema({
-       products:[{
-             product : {
-              type:Object,
-              required:true,
-             },
-             quantity:{
-              type:Schema.Types.Number,
-              required:true,
-             }
-       }],
-       user:{
-        name:{
-          type:String,
-          required:true,
-        },
-        userId:{
-          type:Schema.Types.ObjectId,
-          required:true,
-          ref:'User',
-        }
-       }
+  products: [
+    {
+      product: { type: Object, required: true },
+      quantity: { type: Number, required: true }
+    }
+  ],
+  user: {
+    name: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    }
+  }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
